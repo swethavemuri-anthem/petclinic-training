cdad4589e94fa49350a33bfec4bdf4cfc9bfa8c9
Vivek Kamineni <vivek.kamineni@anthem.com>
2018-06-04 13:32:12 -0400

    petclinic-web

---
+++ petclinic-web/src/main/java/com/anthem/ltss/login/WebConfig.java
@@ -0,0 +1,17 @@
+
+package com.anthem.ltss.login;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/ltss/").setViewName("forward:/index.html");
+	}
+	
+}
