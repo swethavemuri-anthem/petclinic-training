80734f7ed87594ce63a05ebf122dde9428c45b24
Andrew Jo <AF80878@LC02W838ZHTDD>
2018-06-26 17:45:12 -0400

    remaining files

---
+++ platform.client.extension.petclinic/src/main/java/com/atlas/client/extension/petclinic/core/CodeValueTypes.java
@@ -0,0 +1,29 @@
+package com.atlas.client.extension.petclinic.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.antheminc.oss.nimbus.domain.defn.Model.Param.Values.Source;
+import com.antheminc.oss.nimbus.domain.model.config.ParamValue;
+/**
+ * @author Andrew Jo
+ *
+ */
+public class CodeValueTypes {
+	
+    public static class petType implements Source {
+    	@Override
+		public List<ParamValue> getValues(String paramPath) {
+			List<ParamValue> values = new ArrayList<>();
+			values.add(new ParamValue("Cat", "Cat"));
+            values.add(new ParamValue("Dog", "Dog"));
+            values.add(new ParamValue("Parrot", "Parrot"));
+			values.add(new ParamValue("Horse", "Horse"));
+			values.add(new ParamValue("Turtle", "Turle"));
+			values.add(new ParamValue("Yeti", "Yeti"));
+			values.add(new ParamValue("Other", "Other"));
+			return values;
+		}
+    }
+
+}
---
+++ platform.client.extension.petclinic/src/main/java/com/atlas/client/extension/petquestionnaire/core/PetAssessment_Name.java
@@ -0,0 +1,33 @@
+package com.atlas.client.extension.petquestionnaire.core;
+
+import com.antheminc.oss.nimbus.domain.defn.Model;
+import com.antheminc.oss.nimbus.domain.defn.ViewConfig.TextBox;
+import com.antheminc.oss.nimbus.domain.defn.extension.ValidateConditional;
+import com.antheminc.oss.nimbus.domain.defn.extension.Content.Label;
+import com.antheminc.oss.nimbus.domain.defn.extension.ValidateConditional.GROUP_1;
+import com.antheminc.oss.nimbus.domain.defn.extension.ValidateConditional.ValidationScope;
+
+import lombok.Data;
+/**
+ * @author Andrew Jo
+ *
+ */
+@Model
+@Data
+public class PetAssessment_Name {
+	
+	// TextBox
+	// ValidateConditional
+	@TextBox(postEventOnChange = true)
+	@Label(value = "First Name")
+	@ValidateConditional(when = " state != null && state != ''", targetGroup = GROUP_1.class, scope = ValidationScope.SIBLING_NESTED)
+	private String petForm_1a;
+	
+	// TextBox
+		// ValidateConditional
+	@TextBox(postEventOnChange = true)
+	@Label(value = "Last Name")
+	@ValidateConditional(when = "state != null && state != ''", targetGroup = GROUP_1.class, scope = ValidationScope.SIBLING_NESTED)
+	private String petForm_1b;
+
+}
