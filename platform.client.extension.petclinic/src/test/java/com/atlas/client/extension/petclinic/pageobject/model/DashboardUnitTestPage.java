80781dbd97067ab50b7532bb1fcd76c3ad059637
Antonio Lopez <AF42192@lc02sd0tng8wm.local.us.ad.wellpoint.com>
2018-07-09 08:31:33 -0500

    Updating test cases to use framework provided support

--- platform.client.extension.petclinic/src/test/java/com/atlas/client/extension/petclinic/pageobject/model/DashboardUnitTestPage.java
+++ platform.client.extension.petclinic/src/test/java/com/atlas/client/extension/petclinic/pageobject/model/DashboardUnitTestPage.java
@@ -19,8 +19,8 @@ import org.springframework.mock.web.MockHttpServletRequest;
 
 import com.antheminc.oss.nimbus.context.BeanResolverStrategy;
 import com.antheminc.oss.nimbus.domain.cmd.Action;
+import com.antheminc.oss.nimbus.test.domain.support.pageobject.UnitTestPage;
 import com.antheminc.oss.nimbus.test.domain.support.utils.MockHttpRequestBuilder;
-import com.atlas.client.extension.petclinic.pageobject.core.UnitTestPage;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
