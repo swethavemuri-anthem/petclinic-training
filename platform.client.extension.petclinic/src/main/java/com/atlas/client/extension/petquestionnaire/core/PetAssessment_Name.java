8a3be37ab6319e73d511ed534f5e4ebbda4f0ebd
Antonio Lopez <AF42192@lc02sd0tng8wm.local.us.ad.wellpoint.com>
2018-07-12 08:15:58 -0500

    Updating framework version

--- platform.client.extension.petclinic/src/main/java/com/atlas/client/extension/petquestionnaire/core/PetAssessment_Name.java
+++ platform.client.extension.petclinic/src/main/java/com/atlas/client/extension/petquestionnaire/core/PetAssessment_Name.java
@@ -5,7 +5,6 @@ import com.antheminc.oss.nimbus.domain.defn.ViewConfig.TextBox;
 import com.antheminc.oss.nimbus.domain.defn.extension.Content.Label;
 import com.antheminc.oss.nimbus.domain.defn.extension.ValidateConditional;
 import com.antheminc.oss.nimbus.domain.defn.extension.ValidateConditional.GROUP_1;
-import com.antheminc.oss.nimbus.domain.defn.extension.ValidateConditional.ValidationScope;
 
 import lombok.Data;
 /**
@@ -20,14 +19,14 @@ public class PetAssessment_Name {
 	// ValidateConditional
 	@TextBox(postEventOnChange = true)
 	@Label(value = "First Name")
-	@ValidateConditional(when = " state != null && state != ''", targetGroup = GROUP_1.class, scope = ValidationScope.SIBLING_NESTED)
+	@ValidateConditional(when = " state != null && state != ''", targetGroup = GROUP_1.class)
 	private String petForm_1a;
 	
 	// TextBox
 		// ValidateConditional
 	@TextBox(postEventOnChange = true)
 	@Label(value = "Last Name")
-	@ValidateConditional(when = "state != null && state != ''", targetGroup = GROUP_1.class, scope = ValidationScope.SIBLING_NESTED)
+	@ValidateConditional(when = "state != null && state != ''", targetGroup = GROUP_1.class)
 	private String petForm_1b;
 
 }
