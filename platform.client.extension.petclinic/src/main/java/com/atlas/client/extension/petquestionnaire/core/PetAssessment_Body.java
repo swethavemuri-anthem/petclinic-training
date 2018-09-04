f7cc7c0358adebd8d855eb6057c4085dd0d1aca0
Vemuri, Swetha <Swetha.Vemuri@anthem.com>
2018-09-04 09:39:13 -0400

    Petclinic label state changes

--- platform.client.extension.petclinic/src/main/java/com/atlas/client/extension/petquestionnaire/core/PetAssessment_Body.java
+++ platform.client.extension.petclinic/src/main/java/com/atlas/client/extension/petquestionnaire/core/PetAssessment_Body.java
@@ -16,6 +16,7 @@ import com.antheminc.oss.nimbus.domain.defn.ViewConfig.ComboBox;
 import com.antheminc.oss.nimbus.domain.defn.ViewConfig.FileUpload;
 import com.antheminc.oss.nimbus.domain.defn.ViewConfig.Radio;
 import com.antheminc.oss.nimbus.domain.defn.ViewConfig.Signature;
+import com.antheminc.oss.nimbus.domain.defn.ViewConfig.TabInfo;
 import com.antheminc.oss.nimbus.domain.defn.ViewConfig.TextArea;
 import com.antheminc.oss.nimbus.domain.defn.ViewConfig.TextBox;
 import com.antheminc.oss.nimbus.domain.defn.extension.ActivateConditional;
@@ -35,6 +36,7 @@ import com.atlas.client.extension.petquestionnaire.core.CodeValueTypes.VisitReas
 import com.atlas.client.extension.petquestionnaire.core.CodeValueTypes.YNType;
 import com.atlas.client.extension.petquestionnaire.core.CodeValueTypes.YesTest;
 import com.atlas.client.extension.petquestionnaire.core.CodeValueTypes.timeSelection;
+import com.atlas.client.extension.petquestionnaire.core.PetAssessment_Name.PetAssessment_Name_Tab;
 
 import lombok.Data;
 import lombok.Getter;
@@ -47,259 +49,265 @@ import lombok.Setter;
 @Data
 public class PetAssessment_Body {
 	
+	@TabInfo
+	@Label("Pet Assessment Details")
+	private PetAssessment_Body_Tab petAssessment_Accordion_tab;
 	
-	
-	// ActivateConditional
-	// ActivateConditionals
-	// Calendar (date OR time formats)
-	@Label(value = "Q1")
-	private Q1 q1;
-	
-	@Model
-	@Getter
-	@Setter
-	public static class Q1 {
+	@Model @Data 
+	public static class PetAssessment_Body_Tab {
+		// ActivateConditional
+		// ActivateConditionals
+		// Calendar (date OR time formats)
+		@Label(value = "Q1")
+		private Q1 q1;
 		
-		@Radio(postEventOnChange = true) 
-//		@NotNull
-		@Model.Param.Values(value = timeSelection.class)
-		@ActivateConditionals({
-			@ActivateConditional(when = "state == 'Date Only'", targetPath = "/../q1_b"),
-			@ActivateConditional(when = "state == 'Time Only'", targetPath = "/../q1_c"),
-			@ActivateConditional(when = "state == 'Both Date and Time'", targetPath = "/../q1_d")
-		})
-		@Label(value= "1. Choose date/time format for the date of appointment")
-		private String q1_a;
+		@Model
+		@Getter
+		@Setter
+		public static class Q1 {
+			
+			@Radio(postEventOnChange = true) 
+//			@NotNull
+			@Model.Param.Values(value = timeSelection.class)
+			@ActivateConditionals({
+				@ActivateConditional(when = "state == 'Date Only'", targetPath = "/../q1_b"),
+				@ActivateConditional(when = "state == 'Time Only'", targetPath = "/../q1_c"),
+				@ActivateConditional(when = "state == 'Both Date and Time'", targetPath = "/../q1_d")
+			})
+			@Label(value= "1. Choose date/time format for the date of appointment")
+			private String q1_a;
+			
+			@Calendar(postEventOnChange = true)
+			@Label(value = "Date ONLY of Appointment")
+			private LocalDate q1_b;
+			
+			@Calendar(postEventOnChange = true, timeOnly= true)
+			@Label(value = "Time ONLY of Appointment")
+			private LocalDateTime q1_c;
+			
+			@Calendar(postEventOnChange = true, showTime = true)
+			@Label(value = "Date AND Time of Appointment")
+			private LocalDate q1_d;
+		}
 		
-		@Calendar(postEventOnChange = true)
-		@Label(value = "Date ONLY of Appointment")
-		private LocalDate q1_b;
+		// ActivateConditional (multiple)
+		// ComboBox
+		// Radio
+		// TextBox
+		@Label(value="Q2")
+		private Q2 q2;
 		
-		@Calendar(postEventOnChange = true, timeOnly= true)
-		@Label(value = "Time ONLY of Appointment")
-		private LocalDateTime q1_c;
-		
-		@Calendar(postEventOnChange = true, showTime = true)
-		@Label(value = "Date AND Time of Appointment")
-		private LocalDate q1_d;
-	}
-	
-	// ActivateConditional (multiple)
-	// ComboBox
-	// Radio
-	// TextBox
-	@Label(value="Q2")
-	private Q2 q2;
-	
-	@Model
-	@Getter
-	@Setter
-	public static class Q2 {	
-		
-		@Radio(postEventOnChange = true)
-//		@NotNull
-		@Model.Param.Values(value = YNType.class)
-		@ActivateConditional(when = "state == 'Yes'", targetPath = "/../q2_b")
-		@Label(value="2. Was this your first visit at the Pet Clinic?")
-		private String q2_a;
+		@Model
+		@Getter
+		@Setter
+		public static class Q2 {	
+			
+			@Radio(postEventOnChange = true)
+//			@NotNull
+			@Model.Param.Values(value = YNType.class)
+			@ActivateConditional(when = "state == 'Yes'", targetPath = "/../q2_b")
+			@Label(value="2. Was this your first visit at the Pet Clinic?")
+			private String q2_a;
 
-		@ComboBox(postEventOnChange = true)
-		@Model.Param.Values(value = InboundType.class)
-		@ActivateConditional(when = "state == 'Other'", targetPath = "/../q2_c")
-		@Label(value="How did you hear about the Pet Clinic?")
-		private String q2_b;
+			@ComboBox(postEventOnChange = true)
+			@Model.Param.Values(value = InboundType.class)
+			@ActivateConditional(when = "state == 'Other'", targetPath = "/../q2_c")
+			@Label(value="How did you hear about the Pet Clinic?")
+			private String q2_b;
 
-		@TextBox(postEventOnChange = true)
-		@Label(value="Other (please specify)")
-		private String q2_c;
-	}
-	
-	// EnableConditional
-	// TextBox
-	@Label(value="Q3")
-	private Q3 q3;
-	
-	@Model
-	@Getter
-	@Setter
-	public static class Q3 {
+			@TextBox(postEventOnChange = true)
+			@Label(value="Other (please specify)")
+			private String q2_c;
+		}
 		
-        @Radio(postEventOnChange = true)
-		@Path(linked = false)
-//		@NotNull
-        @Model.Param.Values(value = InboundReason.class)
-		@EnableConditional(when = "state == 'Other'", targetPath = "/../q3_b")
-        @Label(value = "3. Why did you choose our Pet Clinic?")
-        private String q3_a;
+		// EnableConditional
+		// TextBox
+		@Label(value="Q3")
+		private Q3 q3;
 		
-		@TextBox(postEventOnChange = true)
-		@Label(value="Other (please specify)")
-		private String q3_b;
-	}
-	
-	// ActivateConditional
-	// Radio
-	// TextBox
-	@Label(value="Q4")
-	private Q4 q4;
-	
-	@Model
-	@Getter
-	@Setter
-	public static class Q4 {
+		@Model
+		@Getter
+		@Setter
+		public static class Q3 {
+			
+	        @Radio(postEventOnChange = true)
+			@Path(linked = false)
+//			@NotNull
+	        @Model.Param.Values(value = InboundReason.class)
+			@EnableConditional(when = "state == 'Other'", targetPath = "/../q3_b")
+	        @Label(value = "3. Why did you choose our Pet Clinic?")
+	        private String q3_a;
+			
+			@TextBox(postEventOnChange = true)
+			@Label(value="Other (please specify)")
+			private String q3_b;
+		}
 		
-		@Radio(postEventOnChange = true)
-//		@NotNull
-        @Model.Param.Values(value = VisitReason.class)
-		@ActivateConditional(when = "state == 'Other'", targetPath = "/../q4_b")
-        @Label(value = "4. What was the reason for your most recent visit to the Pet Clinic?")
-        private String q4_a;
+		// ActivateConditional
+		// Radio
+		// TextBox
+		@Label(value="Q4")
+		private Q4 q4;
 		
-		@TextBox(postEventOnChange = true)
-		@Label(value="Other (please specify)")
-		private String q4_b;
-	}
-	
-	// Radio
-	@Label(value="Q5")
-	private Q5 q5;
-	
-	@Model
-	@Getter
-	@Setter
-	public static class Q5 {	
+		@Model
+		@Getter
+		@Setter
+		public static class Q4 {
+			
+			@Radio(postEventOnChange = true)
+//			@NotNull
+	        @Model.Param.Values(value = VisitReason.class)
+			@ActivateConditional(when = "state == 'Other'", targetPath = "/../q4_b")
+	        @Label(value = "4. What was the reason for your most recent visit to the Pet Clinic?")
+	        private String q4_a;
+			
+			@TextBox(postEventOnChange = true)
+			@Label(value="Other (please specify)")
+			private String q4_b;
+		}
 		
-		@Radio(postEventOnChange = true)
-//		@NotNull
-		@Model.Param.Values(value = YNType.class)
-		@Label(value="5. Were you greeted promptly and politely?")
-		private String q5_a;
-	}
+		// Radio
+		@Label(value="Q5")
+		private Q5 q5;
+		
+		@Model
+		@Getter
+		@Setter
+		public static class Q5 {	
+			
+			@Radio(postEventOnChange = true)
+//			@NotNull
+			@Model.Param.Values(value = YNType.class)
+			@Label(value="5. Were you greeted promptly and politely?")
+			private String q5_a;
+		}
 
-	// String
-	@CheckBoxGroup
-	@Label("Please rate each part of your visit: ")
-	private String surveyLabel;
-	
-	// Radio
-	@Label(value="Q6")
-	private Q6 q6;
-	
-	@Model
-	@Getter
-	@Setter
-	public static class Q6{
+		// String
+		@CheckBoxGroup
+		@Label("Please rate each part of your visit: ")
+		private String surveyLabel;
 		
-		@Radio(postEventOnChange = true)
-//		@NotNull
-		@Model.Param.Values(value = SatisfactionType.class)
-		@Label(value="6. Making an appointment: ")
-		private String q6_a;
-	}
-	
-	// Radio
-	@Label(value="Q7")
-	private Q7 q7;
-	
-	@Model
-	@Getter
-	@Setter
-	public static class Q7{
+		// Radio
+		@Label(value="Q6")
+		private Q6 q6;
 		
-		@Radio(postEventOnChange = true)
-//		@NotNull
-		@Model.Param.Values(value = SatisfactionType.class)
-		@Label(value="7. Checking in: ")
-		private String q7_a;
-	}
-	
-	// Radio
-	@Label(value="Q8")
-	private Q8 q8;
-	
-	@Model
-	@Getter
-	@Setter
-	public static class Q8{
+		@Model
+		@Getter
+		@Setter
+		public static class Q6{
+			
+			@Radio(postEventOnChange = true)
+//			@NotNull
+			@Model.Param.Values(value = SatisfactionType.class)
+			@Label(value="6. Making an appointment: ")
+			private String q6_a;
+		}
 		
-		@Radio(postEventOnChange = true, help="This field requires a value for pet type")
-//		@NotNull
-		@Model.Param.Values(value = SatisfactionType.class)
-		@Label(value="8. Examination of my pet: ")
-		private String q8_a;
-	}
-	
-	// Radio
-	// VisibleConditional
-	// FileUpload
-	@Label(value="Q9")
-	private Q9 q9;
-	
-	@Model
-	@Getter
-	@Setter
-	public static class Q9 {	
+		// Radio
+		@Label(value="Q7")
+		private Q7 q7;
 		
-		@Radio(postEventOnChange = true)
-//		@NotNull
-		@Model.Param.Values(value = YNType.class)
-		@VisibleConditional(when = "state == 'Yes'", targetPath = "/../q9_b")
-		@Label(value="9. Do you have any relevant file/s to upload? (only in .png or .pdf formats)")
-		private String q9_a;
+		@Model
+		@Getter
+		@Setter
+		public static class Q7{
+			
+			@Radio(postEventOnChange = true)
+//			@NotNull
+			@Model.Param.Values(value = SatisfactionType.class)
+			@Label(value="7. Checking in: ")
+			private String q7_a;
+		}
+		
+		// Radio
+		@Label(value="Q8")
+		private Q8 q8;
+		
+		@Model
+		@Getter
+		@Setter
+		public static class Q8{
+			
+			@Radio(postEventOnChange = true, help="This field requires a value for pet type")
+//			@NotNull
+			@Model.Param.Values(value = SatisfactionType.class)
+			@Label(value="8. Examination of my pet: ")
+			private String q8_a;
+		}
+		
+		// Radio
+		// VisibleConditional
+		// FileUpload
+		@Label(value="Q9")
+		private Q9 q9;
+		
+		@Model
+		@Getter
+		@Setter
+		public static class Q9 {	
+			
+			@Radio(postEventOnChange = true)
+//			@NotNull
+			@Model.Param.Values(value = YNType.class)
+			@VisibleConditional(when = "state == 'Yes'", targetPath = "/../q9_b")
+			@Label(value="9. Do you have any relevant file/s to upload? (only in .png or .pdf formats)")
+			private String q9_a;
 
-		@FileUpload(type=".png,.pdf")
-		@Label(value="Please upload the relevant file/s")
-		private String q9_b;
-	}
-	
-	// Radio
-	//TextBox
-	// ValueConditional
-	@Label(value="Q10")
-	private Q10 q10;
-	
-	@Model
-	@Getter @Setter
-	public static class Q10 {
+			@FileUpload(type=".png,.pdf")
+			@Label(value="Please upload the relevant file/s")
+			private String q9_b;
+		}
 		
-		@TextBox(postEventOnChange = true)
-		@ValuesConditional(target = "/../q10_b", condition = {
-			@Condition(when = "state == 'Yes'", then = @Values(YesTest.class)),
-			@Condition(when = "state == 'No'", then = @Values(NoTest.class)),
-			@Condition(when = "state == 'No'", then = @Values(NoTest.class)),
-			@Condition(when = "state != 'Yes' || state != 'No'", then = @Values(NeitherTest.class)),
-		})
-		@Label(value="10. Values Conditional Test (type Yes or No)")
-		private String q10_a;
+		// Radio
+		//TextBox
+		// ValueConditional
+		@Label(value="Q10")
+		private Q10 q10;
 		
-		@Radio
-		@Values(AllTest.class)
-		@Label(value="Value Conditional Test Output: ")
-		private String q10_b;
-	}
-	
-	// CheckBoxGroup (with minimum size limitation)
-	@CheckBoxGroup(postEventOnChange = true)
-	@Size(min = 3, message = "Required number of selections not met (3)")
-	@Model.Param.Values(value = VisitReason.class)
-	@Label(value="11. CheckBoxGroup test (minimum of three):")
-	private String[] q11;
-	
-	
-	
-	// TextArea (with max Characters limitation)
-	@TextArea(postEventOnChange = true)
-	@Label("12. Please feel free to make any other comments, good or bad, "
-	+ "that will allow us to serve you better. "
-	+ "Thanks for your time! We appreciate your comments. (Max 5 Characters)")
-	@Pattern(regexp = "^[\\S\\s]{0,5}$", message = "Max 5 Characters")
-	private String q12;
+		@Model
+		@Getter @Setter
+		public static class Q10 {
+			
+			@TextBox(postEventOnChange = true)
+			@ValuesConditional(target = "/../q10_b", condition = {
+				@Condition(when = "state == 'Yes'", then = @Values(YesTest.class)),
+				@Condition(when = "state == 'No'", then = @Values(NoTest.class)),
+				@Condition(when = "state == 'No'", then = @Values(NoTest.class)),
+				@Condition(when = "state != 'Yes' || state != 'No'", then = @Values(NeitherTest.class)),
+			})
+			@Label(value="10. Values Conditional Test (type Yes or No)")
+			private String q10_a;
+			
+			@Radio
+			@Values(AllTest.class)
+			@Label(value="Value Conditional Test Output: ")
+			private String q10_b;
+		}
+		
+		// CheckBoxGroup (with minimum size limitation)
+		@CheckBoxGroup(postEventOnChange = true)
+		@Size(min = 3, message = "Required number of selections not met (3)")
+		@Model.Param.Values(value = VisitReason.class)
+		@Label(value="11. CheckBoxGroup test (minimum of three):")
+		private String[] q11;
+		
+		
+		
+		// TextArea (with max Characters limitation)
+		@TextArea(postEventOnChange = true)
+		@Label("12. Please feel free to make any other comments, good or bad, "
+		+ "that will allow us to serve you better. "
+		+ "Thanks for your time! We appreciate your comments. (Max 5 Characters)")
+		@Pattern(regexp = "^[\\S\\s]{0,5}$", message = "Max 5 Characters")
+		private String q12;
 
-	// Signature
-	// Not Null (with custom message)
-	@Signature(postEventOnChange=true)
-	@IgnoreCopy
-	@NotNull (message = "The submission requires an acknowledgement signature")
-	@Label(value = "13. Please provide signature:")
-	private String q13;
+		// Signature
+		// Not Null (with custom message)
+		@Signature(postEventOnChange=true)
+		@IgnoreCopy
+		@NotNull (message = "The submission requires an acknowledgement signature")
+		@Label(value = "13. Please provide signature:")
+		private String q13;
+	}
+	
 }
--- platform.client.extension.petclinic/src/main/java/com/atlas/client/extension/petquestionnaire/core/PetAssessment_Footer.java
+++ platform.client.extension.petclinic/src/main/java/com/atlas/client/extension/petquestionnaire/core/PetAssessment_Footer.java
@@ -2,6 +2,7 @@ package com.atlas.client.extension.petquestionnaire.core;
 
 import com.antheminc.oss.nimbus.domain.defn.Model;
 import com.antheminc.oss.nimbus.domain.defn.ViewConfig.Button;
+import com.antheminc.oss.nimbus.domain.defn.ViewConfig.TabInfo;
 import com.antheminc.oss.nimbus.domain.defn.extension.AccessConditional;
 import com.antheminc.oss.nimbus.domain.defn.extension.AccessConditional.Permission;
 import com.antheminc.oss.nimbus.domain.defn.extension.Content.Label;
@@ -15,16 +16,19 @@ import lombok.Data;
 @Data
 public class PetAssessment_Footer {
 
-	// Button
-	// AccessConditional
-	@AccessConditional(whenAuthorities="?[#this == 'TEST'].empty", p=Permission.HIDDEN)
-	@Button(style = Button.Style.PRIMARY, type = Button.Type.submit)
-	@Label(value = "Submit")
-	private String submit;
+	@TabInfo
+	private PetAssessment_Footer_Tab footer_tab;
 	
-	// Button
-	@Button(browserBack = true)
-	@Label(value = "Back")
-	private String back;
+	@Model @Data
+	public static class PetAssessment_Footer_Tab {
+		@AccessConditional(whenAuthorities="?[#this == 'TEST'].empty", p=Permission.HIDDEN)
+		@Button(style = Button.Style.PRIMARY, type = Button.Type.submit)
+		@Label(value = "Submit")
+		private String submit;
+		
+		@Button(browserBack = true)
+		@Label(value = "Back")
+		private String back;
+	}
 	
 }
