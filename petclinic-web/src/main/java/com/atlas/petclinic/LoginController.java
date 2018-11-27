ee664c0e1ca4956fda602b0d07dce5992fb162bb
Vemuri, Swetha <Swetha.Vemuri@anthem.com>
2018-11-27 12:00:25 -0500

    Cleanup

--- petclinic-web/src/main/java/com/atlas/petclinic/LoginController.java
+++ petclinic-web/src/main/java/com/atlas/petclinic/LoginController.java
@@ -3,24 +3,12 @@
  */
 package com.atlas.petclinic;
 
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import com.antheminc.oss.nimbus.domain.cmd.exec.ExecuteOutput.GenericExecute;
-import com.antheminc.oss.nimbus.entity.client.user.ClientUser;
-import com.antheminc.oss.nimbus.support.JustLogit;
 
 
 /**
@@ -30,29 +18,10 @@ import com.antheminc.oss.nimbus.support.JustLogit;
 @Controller
 public class LoginController {
 	
-	@Autowired
-	private RestTemplate restTemplate;
-
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public void login(HttpServletRequest request, HttpServletResponse response) throws Exception {
     	response.sendRedirect("/petclinic/#/h/petclinicdashboard/vpDashboard");
         
     }
-    
-    @RequestMapping(value="/test", method= RequestMethod.GET)
-    public void test() {
-    	UriComponentsBuilder builder = UriComponentsBuilder
-			    .fromUriString("http://localhost:8082/petclinic/client/org/app/p/clientuser/_search")
-			    .queryParam("fn", "query")
-			    .queryParam("where", "clientuser.loginId.in('asnowball')");
-		
-		
-		ResponseEntity<GenericExecute<List<ClientUser>>> multiOutputResp = restTemplate.exchange(builder.toUriString(), HttpMethod.GET, 
-				null, new ParameterizedTypeReference<GenericExecute<List<ClientUser>>>() {});
-		
-		GenericExecute<List<ClientUser>> multiOutputRespBody = multiOutputResp.getBody();
-		
-    }
-   
-    private JustLogit _logger = new JustLogit(this.getClass());
+
 }
--- petclinic-web/src/main/java/com/atlas/petclinic/PetclinicWebApplication.java
+++ petclinic-web/src/main/java/com/atlas/petclinic/PetclinicWebApplication.java
@@ -6,14 +6,13 @@ package com.atlas.petclinic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.antheminc.oss.nimbus.domain.session.HttpSessionProvider;
 import com.antheminc.oss.nimbus.domain.session.SessionProvider;
@@ -26,15 +25,12 @@ import com.antheminc.oss.nimbus.domain.session.SessionProvider;
 @SpringBootApplication(scanBasePackageClasses=LoginController.class)
 @ComponentScan
 @EnableAutoConfiguration
-//@EnableDiscoveryClient
-@RibbonClient(name="owner")
-@EnableEurekaClient
-@RestController
-public class PetclinicWebApplication /*extends SpringBootServletInitializer*/ {
-//	@Override
-//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){ 
-//		return application.sources(PetclinicWebApplication.class);
-//	} 
+public class PetclinicWebApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){ 
+		return application.sources(PetclinicWebApplication.class);
+	} 
+	
 	public static void main(String[] args) throws Exception { 
 		SpringApplication.run(PetclinicWebApplication.class, args);
 	} 
